e583ba417748e666e425d4aaaa0b72d41c568e44
pall <paulallton@gmail.com>
2018-02-02 18:41:33 +0000

    start to put some infrastructure in

---
+++ src/main/scala/im/mange/flakeless/scalatest/WebSpec.scala
@@ -0,0 +1,38 @@
+package im.mange.flakeless.scalatest
+
+import im.mange.flakeless.Flakeless
+import org.scalatest.{FreeSpec, Matchers, Outcome, TestSuite}
+import org.scalatest.refspec.RefSpec
+
+trait WebSpec extends TestSuite {
+  private val _currentTestName = new ThreadLocal[String]
+  private val suite = this.suiteId.split("\\.").reverse.head
+
+  override def withFixture(test: NoArgTest): Outcome = {
+    _currentTestName.set(test.name)
+    val outcome = super.withFixture(test)
+    _currentTestName.set(null)
+    outcome
+  }
+
+  protected def currentTestName: String = {
+    val testName = _currentTestName.get()
+    assert(testName != null, "currentTestName should only be called in a test")
+    testName
+  }
+
+  def testInBrowser(testBody: Flakeless => Unit): Unit = {
+    println(suite + ":" + currentTestName)
+//   val flakeless = Flakeless(null)
+//   testBody(flakeless)
+    testBody(null)
+ }
+}
+
+class ExampleSpec extends RefSpec with WebSpec {
+  def `blah blah` = testInBrowser(flakeless => {
+    //flakeless
+    //    true mustBe true
+    println("I've run")
+  })
+}
