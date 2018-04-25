9d83b41b64974d9392a8c45fcb502dd57006220f
pall <paulallton@gmail.com>
2018-04-25 16:21:30 +0100

    tidy up the pool

--- src/main/scala/im/mange/flakeless/scalatest/SystemUnderTestPool.scala
+++ src/main/scala/im/mange/flakeless/scalatest/SystemUnderTestPool.scala
@@ -3,18 +3,11 @@ package im.mange.flakeless.scalatest
 import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
 import collection.mutable.ListBuffer
 
-//import scala.concurrent.Future
-import scala.concurrent.Future
-
 case class SystemUnderTestPool[T](private val suts: List[T])  {
   private val all = new ListBuffer[T]
   private val available = new LinkedBlockingQueue[T]
 
   suts.foreach(add)
-
-//  def add(browser: BrowserType, instances: Int = 1) {
-//    times(instances) { add(BrowserFactory.create(browser)) }
-//  }
 
   def write(systemUnderTest: T) {
     available.put(systemUnderTest)
@@ -25,27 +18,6 @@ case class SystemUnderTestPool[T](private val suts: List[T])  {
   }
 
   def status = s"all: $all\navailable: $all"
-
-//  def fill() {
-//    Future {
-//      //TODO: add BrowserControllers to the println
-//      println("### Warming up browsers ... " + browserType)
-//      add(browserType, browserInstances)
-//    }
-//  }
-
-//  def empty() {
-//    //TODO: track how long fill and empty take
-//    allBrowsers.par.map(browser => {
-//      try {
-//        Tracker.add(BrowserCloseRequested(browser.id))
-//        browser.close()
-//        Tracker.add(BrowserClosed(browser.id))
-//      } catch {
-//        case e: Exception => Tracker.add(BrowserCloseFailed(browser.id))
-//      }
-//    })
-//  }
 
   private def add(sut: T) {
     all.append(sut)
